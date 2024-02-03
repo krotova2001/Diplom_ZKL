1e0c43b772c9b432378c7c8630b3b3411ba97bd0
Григорий Зимин <griefz@ya.ru>
2024-02-03 19:13:08 +0700

    Добавлены плагины для Entity Postgre. Соеденение с базой данных.

---
+++ DIplom_ZKL.Server/DiplomContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DIplom_ZKL.Server;
+
+public partial class DiplomContext : DbContext
+{
+    public DiplomContext()
+    {
+    }
+
+    public DiplomContext(DbContextOptions<DiplomContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=176.116.170.251;Port=5432;Database=Diplom;Username=krotov;Password=avotorkkkk4321");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("users_pkey");
+
+            entity.ToTable("users", tb => tb.HasComment("Пользователи системы"));
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.IsAdmin)
+                .HasDefaultValue(false)
+                .HasColumnName("is_admin");
+            entity.Property(e => e.Login)
+                .HasColumnType("character varying")
+                .HasColumnName("login");
+            entity.Property(e => e.Name)
+                .HasColumnType("character varying")
+                .HasColumnName("name");
+            entity.Property(e => e.Password)
+                .HasColumnType("character varying")
+                .HasColumnName("password");
+            entity.Property(e => e.Picture).HasColumnName("picture");
+            entity.Property(e => e.Role).HasColumnName("role");
+            entity.Property(e => e.Surname)
+                .HasColumnType("character varying")
+                .HasColumnName("surname");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DIplom_ZKL.Server/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DIplom_ZKL.Server;
+
+/// <summary>
+/// Пользователи системы
+/// </summary>
+public partial class User
+{
+    public Guid Id { get; set; }
+
+    public string Login { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Surname { get; set; } = null!;
+
+    public byte[]? Picture { get; set; }
+
+    public int? Role { get; set; }
+
+    public bool? IsAdmin { get; set; }
+}
