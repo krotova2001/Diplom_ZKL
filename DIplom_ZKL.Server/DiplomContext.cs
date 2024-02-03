2157a7d30275960f6c098395364223146378bb15
Григорий Зимин <griefz@ya.ru>
2024-02-03 20:43:56 +0700

    Перенесена строка подключения в appsettings.json

--- DIplom_ZKL.Server/DiplomContext.cs
+++ DIplom_ZKL.Server/DiplomContext.cs
@@ -16,10 +16,9 @@ public partial class DiplomContext : DbContext
     }
 
     public virtual DbSet<User> Users { get; set; }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=176.116.170.251;Port=5432;Database=Diplom;Username=krotov;Password=avotorkkkk4321");
+        => optionsBuilder.UseNpgsql(new ConfigurationBuilder().Build().GetConnectionString("constring"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
